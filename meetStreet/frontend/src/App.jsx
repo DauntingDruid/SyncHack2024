69ec4c7a8528e4f9e54aed3549755e8f61df2876
DauntingDruid <abhishekroxtomar@gmail.com>
2024-08-24 20:06:12 +1000

    tailwind + homepage

--- meetStreet/frontend/src/App.jsx
+++ meetStreet/frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import HomePage from './pages/home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Welcome to Meet Street!</h1>
-    </>
+    <div className='w-full h-full flex justify-center items-center'>
+        <HomePage />
+    </div>
   )
 }
 
---
+++ meetStreet/frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure Tailwind processes all files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
